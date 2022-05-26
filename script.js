5e5e8ff1d143359a71de2dc88cc09cc4d3649168
Fernando Abiel Gutiérrez Chávez <91234211+Fer0605@users.noreply.github.com>
2022-05-25 19:40:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/serviceworker.js')
+    navigator.serviceWorker.register('https://Fer0605.github.io/Curriculum/serviceworker.js')
     .then(reg => console.log(reg))
     .catch(error => console.warn(error))
 }
